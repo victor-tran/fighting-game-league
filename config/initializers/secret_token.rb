31afdc0e7dabdf24cb381deb0889bef321094d2a
Victor Tran <victor.tran91@gmail.com>
2014-01-21 19:29:29 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FightingGameLeague::Application.config.secret_key_base = '15cb0ee8a81319b22748951cfc71ea69a0c53162ca0516123fc52de2deff0d8ae27e76fe54d465100d07f60f988841df824e136ebd9364a278069d4285632637'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FightingGameLeague::Application.config.session_store :cookie_store, key: '_fighting-game-league_session'
