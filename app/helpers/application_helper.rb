d36f94480ec868a6bc2af6b53df2782b34b520b2
Victor Tran <victor.tran91@gmail.com>
2014-02-20 10:19:54 -0500

    Added bet information for matches show page
    You can see how many people (and percentages) bet on each player

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -88,6 +88,22 @@ module ApplicationHelper
     end
   end
 
+  # Returns bet info for match.
+  def match_bet_info(match)
+    p1_bet_count = Bet.where("match_id = ? AND favorite_id = ?", match.id, match.p1_id).count
+    p2_bet_count = Bet.where("match_id = ? AND favorite_id = ?", match.id, match.p2_id).count
+
+    p1_percent  = (p1_bet_count.to_f / (p1_bet_count + p2_bet_count)) * 100
+    p2_percent = (p2_bet_count.to_f / (p1_bet_count + p2_bet_count)) * 100
+
+    # e.g. ( 0 ) 0% | 100% ( 1 )
+    "( " + p1_bet_count.to_s + " ) " + 
+    number_to_percentage(p1_percent, precision: 0) + 
+    " | " + 
+    number_to_percentage(p2_percent, precision: 0) + 
+    "( " + p2_bet_count.to_s + " )"
+  end
+
   # Returns overall W-L fighter history.
 
   # Returns current match streak.
