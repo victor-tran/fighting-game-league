b01a83451a06b709efad7ef18c8c945e2a3d2938
Victor Tran <victor.tran91@gmail.com>
2014-02-20 12:53:48 -0500

    Made League/Memberships association destroy dependent

--- app/models/league.rb
+++ app/models/league.rb
@@ -1,5 +1,5 @@
 class League < ActiveRecord::Base
-  has_many :memberships
+  has_many :memberships, dependent: :destroy
   has_many :users, through: :memberships
   has_many :matches, dependent: :destroy
 
