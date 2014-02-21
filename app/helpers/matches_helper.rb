f5f8417ed2156d039e51009bf5a7b00386655796
Victor Tran <victor.tran91@gmail.com>
2014-02-21 10:35:14 -0500

    Fixed match_bet_info when there were no bets placed

--- app/helpers/matches_helper.rb
+++ app/helpers/matches_helper.rb
@@ -5,8 +5,13 @@ module MatchesHelper
     p1_bet_count = Bet.where("match_id = ? AND favorite_id = ?", match.id, match.p1_id).count
     p2_bet_count = Bet.where("match_id = ? AND favorite_id = ?", match.id, match.p2_id).count
 
-    p1_percent  = (p1_bet_count.to_f / (p1_bet_count + p2_bet_count)) * 100
-    p2_percent = (p2_bet_count.to_f / (p1_bet_count + p2_bet_count)) * 100
+    if p1_bet_count == 0 && p2_bet_count == 0
+      p1_percent = 0
+      p2_percent = 0
+    else 
+      p1_percent  = (p1_bet_count.to_f / (p1_bet_count + p2_bet_count)) * 100
+      p2_percent = (p2_bet_count.to_f / (p1_bet_count + p2_bet_count)) * 100
+    end
 
     # e.g. ( 0 ) 0% | 100% ( 1 )
     "( " + p1_bet_count.to_s + " ) " + 
