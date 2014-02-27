cb84ac175e74447f612e4780cedba1ebdd4bb6a6
Victor Tran <victor.tran91@gmail.com>
2014-02-27 08:39:38 -0500

    Put generate_user_standings back into league_helper
    Needed it so that we could pass in different set of matches.

--- app/models/league.rb
+++ app/models/league.rb
@@ -75,45 +75,6 @@ class League < ActiveRecord::Base
     current_matches
   end
 
-  # Returns an array sorted by Wins that contains each user's W-L-MP.
-  def generate_user_standings
-
-    # Set all user's W-L values to 0-0 and set matches played to 0.
-    user_hashmap = {}
-    users.each do |user|
-      user_hashmap[user] = [0,0,0]
-    end
-
-    match_set = matches_for_current_season
-
-    # Calculate each user's W-L-MP
-    match_set.each do |match|
-
-      # Only calculate if match has been accepted by p1 & p2.
-      if match.p1_accepted == true && match.p2_accepted == true
-        if match.p1_id == match.winner_id
-          user_hashmap[User.find(match.p1_id)][0] += 1
-          user_hashmap[User.find(match.p1_id)][2] += 1
-
-          user_hashmap[User.find(match.p2_id)][1] += 1
-          user_hashmap[User.find(match.p2_id)][2] += 1
-        else
-          user_hashmap[User.find(match.p2_id)][0] += 1
-          user_hashmap[User.find(match.p2_id)][2] += 1
-
-          user_hashmap[User.find(match.p1_id)][1] += 1
-          user_hashmap[User.find(match.p1_id)][2] += 1
-        end
-
-        # Remove the match from match_set
-        match_set.delete(match)
-      end
-    end
-
-    # Convert hashmap to an array that is sorted by Wins and then by MP.
-    user_hashmap.to_a.sort_by{ |user| [ -user[1][0], -user[1][2] ] }
-  end
-
   # Generates and schedules matches for a single round-robin schedule.
   def generate_matches
     
