9a9f45060d3f14bbb5aa19a8263f82cb6903811f
Victor Tran <victor.tran91@gmail.com>
2014-03-26 15:56:54 -0400

    Added validations to Bet model.
    Thanks testing!

--- app/models/bet.rb
+++ app/models/bet.rb
@@ -3,5 +3,8 @@ class Bet < ActiveRecord::Base
   belongs_to :better, class_name: 'User'
   belongs_to :favorite, class_name: 'User'
 
+  validates :match_id, presence: true
+  validates :better_id, presence: true
+  validates :favorite_id, presence: true
   validates_numericality_of :wager_amount, :greater_than => 0
 end
--- spec/models/bet_spec.rb
+++ spec/models/bet_spec.rb
@@ -1,8 +1,24 @@
 require 'spec_helper'
 
 describe Bet do
-  it { should respond_to(:wager_amount) }
+
+  before do
+    @bet = Bet.new(
+              match_id: 1, 
+              better_id: 1, 
+              favorite_id: 2,
+              wager_amount: 1)
+  end
+
+  subject { @bet }
+
   it { should respond_to(:match) }
+  it { should respond_to(:match_id) }
   it { should respond_to(:better) }
+  it { should respond_to(:better_id) }
   it { should respond_to(:favorite) }
+  it { should respond_to(:favorite_id) }
+  it { should respond_to(:wager_amount) }
+
+  it { should be_valid }
 end
