52f4f3024016f0d420b5ff4434598bdb1ba6849b
Victor Tran <victor.tran91@gmail.com>
2014-05-12 15:35:52 -0400

    Made following users button update via AJAX
    Also made user’s league list scrollable and the logo now appears to the
    left of the league’s name. Also fixed a bug where user variable wasn’t
    being passed to follow_league partial

--- app/models/league.rb
+++ app/models/league.rb
@@ -49,6 +49,7 @@ class League < ActiveRecord::Base
   # Banner stuff.
   has_attached_file :banner, 
     styles: {
+      tiny:  '20x20#', 
       thumb: '80x80#',
       large: '1140x275#'
     }, 
