5eac5a8c355c24d7f46632c7121305b8d03c7a7f
Victor Tran <victor.tran91@gmail.com>
2014-05-30 23:35:10 -0400

    Fixed bug
    When a user tries joining/leaving after a league has started.

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -4,34 +4,31 @@ class MembershipsController < ApplicationController
   def create
     @league = League.find(params[:membership][:league_id])
 
-    unless @league.started == true
-      if @league.password_protected 
-        if @league.authenticate(params[:membership][:password])
-          current_user.join!(@league)
-          flash[:notice] = "Joined " + @league.name
-          redirect_to @league
-        else
-          flash[:warning] = "Invalid password"
-          redirect_to join_password_league_path(@league)
-        end
+    if @league.password_protected 
+      if @league.authenticate(params[:membership][:password])
+        current_user.join!(@league) unless @league.started
+        flash[:notice] = "Joined " + @league.name
+        redirect_to @league
       else
-        current_user.join!(@league)
-        respond_to do |format|
-          format.html { redirect_to @league }
-          format.js
-        end
+        flash[:warning] = "Invalid password"
+        redirect_to join_password_league_path(@league)
+      end
+    else
+      current_user.join!(@league) unless @league.started
+      respond_to do |format|
+        format.html { redirect_to @league }
+        format.js
       end
     end
   end
 
   def destroy
     @league = Membership.find(params[:id]).league
-    unless @league.started == true
-      current_user.leave!(@league)
-      respond_to do |format|
-        format.html { redirect_to @league }
-        format.js
-      end
+    
+    current_user.leave!(@league) unless @league.started
+    respond_to do |format|
+      format.html { redirect_to @league }
+      format.js
     end
   end
 end
