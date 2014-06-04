fc0d4415ef6bca9b10d83f376c241ab804fb8952
Victor Tran <victor.tran91@gmail.com>
2014-06-04 12:29:02 -0400

    Added push notification for joining a league.

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -7,6 +7,31 @@ class MembershipsController < ApplicationController
     if @league.password_protected 
       if @league.authenticate(params[:membership][:password])
         current_user.join!(@league) unless @league.started
+        # Create a notification on the backend.
+        n = @league.commissioner.notifications.create!(sendable_id: current_user.id,
+                                                       sendable_type: 'User',
+                                                       targetable_id: @league.id,
+                                                       targetable_type: 'League',
+                                                       action: "joined",
+                                                       read: false)
+        # Send a push notification via Pusher API to @user.
+        if current_user.avatar_file_name == nil
+          Pusher['private-user-'+@league.commissioner.id.to_s].trigger('new_follower_notification',
+                                                         { follower_id: current_user.id,
+                                                           unread_count: @league.commissioner.notifications.unread.count,
+                                                           notification_content: "#{current_user.alias} joined your '#{@league.name}' league.",
+                                                           no_avatar: true,
+                                                           notification_id: n.id })
+        else
+          Pusher['private-user-'+@league.commissioner.id.to_s].trigger('new_follower_notification',
+                                                         { follower_id: current_user.id,
+                                                           unread_count: @league.commissioner.notifications.unread.count,
+                                                           notification_content: "#{current_user.alias} joined your '#{@league.name}' league.",
+                                                           no_avatar: false,
+                                                           img_alt: current_user.avatar_file_name.gsub(".jpg", ""),
+                                                           img_src: current_user.avatar.url(:post),
+                                                           notification_id: n.id })
+        end
         flash[:notice] = "Joined " + @league.name
         redirect_to @league
       else
@@ -15,6 +40,31 @@ class MembershipsController < ApplicationController
       end
     else
       current_user.join!(@league) unless @league.started
+      # Create a notification on the backend.
+      n = @league.commissioner.notifications.create!(sendable_id: current_user.id,
+                                                     sendable_type: 'User',
+                                                     targetable_id: @league.id,
+                                                     targetable_type: 'League',
+                                                     action: "joined",
+                                                     read: false)
+      # Send a push notification via Pusher API to @user.
+      if current_user.avatar_file_name == nil
+        Pusher['private-user-'+@league.commissioner.id.to_s].trigger('new_follower_notification',
+                                                       { follower_id: current_user.id,
+                                                         unread_count: @league.commissioner.notifications.unread.count,
+                                                         notification_content: "#{current_user.alias} joined your '#{@league.name}' league.",
+                                                         no_avatar: true,
+                                                         notification_id: n.id })
+      else
+        Pusher['private-user-'+@league.commissioner.id.to_s].trigger('new_follower_notification',
+                                                       { follower_id: current_user.id,
+                                                         unread_count: @league.commissioner.notifications.unread.count,
+                                                         notification_content: "#{current_user.alias} joined your '#{@league.name}' league.",
+                                                         no_avatar: false,
+                                                         img_alt: current_user.avatar_file_name.gsub(".jpg", ""),
+                                                         img_src: current_user.avatar.url(:post),
+                                                         notification_id: n.id })
+      end
       respond_to do |format|
         format.html { redirect_to @league }
         format.js
@@ -24,8 +74,23 @@ class MembershipsController < ApplicationController
 
   def destroy
     @league = Membership.find(params[:id]).league
-    
     current_user.leave!(@league) unless @league.started
+    # Delete @user's 'joined' notification on the backend.
+    n = @league.commissioner.notifications.find_by(sendable_id: current_user.id,
+                                                   sendable_type: 'User',
+                                                   targetable_id: @league.id,
+                                                   targetable_type: 'League',
+                                                   action: "joined").destroy
+    # Delete the 'joined' notification from @user's list of notifications.
+    Pusher['private-user-'+@league.commissioner.id.to_s].trigger('delete_notification',
+                                               { unread_count: @league.commissioner.notifications.unread.count,
+                                                 notification_id: n.id })
+    # Delete @user's 'followed' notification on the backend.
+    n = @league.commissioner.notifications.find_by(sendable_id: current_user.id,
+                                                   sendable_type: 'User',
+                                                   targetable_id: @league.id,
+                                                   targetable_type: 'League',
+                                                   action: "followed").destroy
     respond_to do |format|
       format.html { redirect_to @league }
       format.js
