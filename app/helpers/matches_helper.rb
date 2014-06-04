5e88e309e98ba57093d33dc981c9270e97e6bbf2
Victor Tran <victor.tran91@gmail.com>
2014-06-03 20:39:11 -0400

    Added bet bar to match show page.

--- app/helpers/matches_helper.rb
+++ app/helpers/matches_helper.rb
@@ -2,6 +2,7 @@ module MatchesHelper
 
   # Returns bet info for match.
   def match_bet_info(match)
+    player_bet_info = {}
     p1_bet_count = Bet.where("match_id = ? AND favorite_id = ?", match.id, match.p1_id).count
     p2_bet_count = Bet.where("match_id = ? AND favorite_id = ?", match.id, match.p2_id).count
 
@@ -13,11 +14,15 @@ module MatchesHelper
       p2_percent = (p2_bet_count.to_f / (p1_bet_count + p2_bet_count)) * 100
     end
 
-    # e.g. ( 0 ) 0% | 100% ( 1 )
-    "( " + p1_bet_count.to_s + " ) " + 
-    number_to_percentage(p1_percent, precision: 0) + 
-    " | " + 
-    number_to_percentage(p2_percent, precision: 0) + 
-    "( " + p2_bet_count.to_s + " )"
+    player_bet_info[:p1_bet_count] = p1_bet_count
+    player_bet_info[:p2_bet_count] = p2_bet_count
+    player_bet_info[:p1_percent] = p1_percent
+    player_bet_info[:p2_percent] = p2_percent
+    player_bet_info[:p1_fight_bucks] = match.bets.sum(:wager_amount, conditions: {favorite_id: [match.p1_id]})
+    player_bet_info[:p2_fight_bucks] = match.bets.sum(:wager_amount, conditions: {favorite_id: [match.p2_id]})
+    #player_bet_info[:p1_percent] = number_to_percentage(p1_percent, precision: 0)
+    #player_bet_info[:p2_percent] = number_to_percentage(p2_percent, precision: 0)
+
+    player_bet_info
   end
 end
