67863b4565142fb0e295749558652f4e4f25ddc5
Victor Tran <victor.tran91@gmail.com>
2014-06-12 23:29:38 -0400

    Completely rewrote round robin scheduling algorithm.
    Didn’t work for more than 4 person leagues.

--- app/models/league.rb
+++ app/models/league.rb
@@ -109,59 +109,50 @@ class League < ActiveRecord::Base
   # Generates and schedules matches for a single round-robin schedule.
   def generate_matches
     
-    matches_per_round = users.count / 2
+    # Shuffle the users in a random order before scheduling.
+    arr = users.to_a.shuffle
 
-    # Generate all possible combinations of head-to-head matches.
-    # Each match in the match array will be an array with 2 users.
-    # Player 1 = match[0] and Player 2 = match[1]
-    match_array = users.combination(2).to_a
+    # Pick the first element in the array as the pivot.
+    pivot = arr.delete_at(0)
 
-    # The hashmap that will keep track of who has already been
-    # scheduled to play for that round.
-    user_hashmap = {}
+    # Schedule matches round by round
+    for current_round in 1..total_rounds
 
-    # Schedule matches round by round, by selecting a random match out of 
-    # the set and making sure that either one of the players hasn't already
-    # been scheduled to play for that round through our generated hashmap.
-    for i in 1..total_rounds
+      # Schedule the match with the pivot and the last element in arr.
+      matches.create!(round_number: current_round, 
+                      p1_id: pivot.id, 
+                      p2_id: arr[-1].id, 
+                      p1_score: 0,
+                      p2_score: 0,
+                      season_id: current_season.id,
+                      league_id: id,
+                      p1_accepted: false,
+                      p2_accepted: false,
+                      game_id: game_id)
 
-      # Will reset all user's scheduled values to false before each round of 
-      # scheduling.
-      users.each do |user|
-        user_hashmap[user] = false
-      end
+      if users.count > 2
+        # Set the index variables for inner loop.
+        i = 0
+        j = arr.count - 2
+        while j >= (arr.count / 2)
 
-      # Schedule matches for this round.
-      for j in 1..matches_per_round
-
-        # Select a random match
-        match = match_array.sample
-
-        # Keep choosing a random match out of the match_array until we get two
-        # teams that haven't been scheduled for this round.
-        until user_hashmap[match[0]] == false && user_hashmap[match[1]] == false
-          match = match_array.sample
+          # Schedule the match with the
+          matches.create!(round_number: current_round, 
+                          p1_id: arr[i].id, 
+                          p2_id: arr[j].id, 
+                          p1_score: 0,
+                          p2_score: 0,
+                          season_id: current_season.id,
+                          league_id: id,
+                          p1_accepted: false,
+                          p2_accepted: false,
+                          game_id: game_id)
+          i += 1
+          j -= 1
         end
 
-        # Create and assign the match for this current round
-        matches.create!(round_number: i, 
-                        p1_id: match[0].id, 
-                        p2_id: match[1].id, 
-                        p1_score: 0,
-                        p2_score: 0,
-                        season_id: current_season.id,
-                        league_id: id,
-                        p1_accepted: false,
-                        p2_accepted: false,
-                        game_id: game_id)
-
-        # Mark the users in the hashmap as TRUE so they won't be scheduled
-        # more than once per round.
-        user_hashmap[match[0]] = true
-        user_hashmap[match[1]] = true
-
-        # Remove the match out of the match_array
-        match_array.delete(match)
+        # Now rotate the array to keep scheduling in a "round robin" fashion.
+        arr.rotate!
       end
     end
   end
